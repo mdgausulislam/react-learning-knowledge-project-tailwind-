0824fee7ee109832e035ee58313aa04d56bd3683
unknown <gausulislam5@gmail.com>
2023-10-07 18:15:40 +0600

    update section

--- src/components/Bookmark/Bookmark.jsx
+++ src/components/Bookmark/Bookmark.jsx
@@ -1,17 +1,13 @@
-import PropTypes from 'prop-types';
+import React from "react";
 
-const Bookmark = ({bookmark}) => {
-    const {title} = bookmark;
+const Bookmark = (props) => {
+    const {title} = props.bookmark;
     return (
         <div className='bg-slate-200 p-4 m-4 rounded-xl'>
-            <h3 className='text-2xl'>{title}</h3>
+            <h3 className='text-4xl'>{title}</h3>
         </div>
     );
 };
-
-Bookmark.propTypes = {
-    bookmark: PropTypes.object
-}
 
 export default Bookmark;
 
